ae8d89664b15b9241f67bfa122fe3cc828ef2564
Efecan Pınar <48981528+efecanpinar@users.noreply.github.com>
2021-06-15 12:21:28 +0300

    Add app files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Burger from "./components/Burger/Burger";
+import Menu from "./components/Menu/Menu";
+
+function App() {
+  return (
+    <div className="App">
+      <Burger />
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
